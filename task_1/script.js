452ff70d2897a8eb26a00c8ffc3d116cde9fca70
maciejkucharski <maciej.kucharski@senti1.com>
2020-06-08 11:43:14 +0200

    init

---
+++ task_1/script.js
@@ -0,0 +1,28 @@
+// 1. Z wykorzystaniem fetch API pobierz dane znajdujące się pod adresem: https://randomuser.me/api/
+// i wyświetl w konsoli imie i nazwisko pobranego użytkownika
+
+const url = 'https://randomuser.me/api/';
+
+
+
+// 2. Z wykorzystaniem fetch API pobierz dane znajdujące się pod adresem https://randomuser.me/api/
+// Ze zwróconego obiektu wykorzystaj pole picture i wyświetl na ekranie avatar użytkownika (do tagu <img>).
+
+
+
+
+
+//3. Z wykorzystaniem fetch API spróbuj pobrać jakieś dane z nieistniejącego adresu URL np. http://testtest
+// Z wiadomych względów takie zapytanie nie powinno się udać, jednak powiadom o tym usera w przyzwoity sposób,
+// Wyświetl komunikat, że niestety, coś jest nie tak, żeby spróbował później.
+
+
+
+
+// 4. W czasie oczekiwania na przyjście danych z serwera, warto dać znać użytkownikowi, że coś sie dzieje,
+// że coś się ładuje. Wyświetl loader, lub komunikat "loading" w czasie kiedy Promise z FetchAPI jest w stanie
+// "Pending". Mozesz wykorzytac takie fajne loadery: https://loading.io/css/
+
+
+
+
---
+++ task_2/script.js
@@ -0,0 +1,12 @@
+// 1. Napisz funkcje o nazwe CompareToTen. Funkcja ta, będzie przyjmowała dowolną liczbę.
+// Funckcja ta będzie zwracać Promise. W implementacji Promise będzie znajdowało się porównanie przekazanej
+// w parametrze liczby do liczby 10. Jeśli przekazana liczba będzie równa (===) 10, wtedy Promise powinien zakończyć się sukcesem
+// W każdym przeciwnym przypadku Promise powinien zakończyć się reject(em).
+
+
+
+// 2. Napisz Promise, który będzie przechodził w stan fulfilled, wtedy kiedy user przyciśnie określony przycisk dodany na stronie
+// Jednak, user bedzię miał tylko 10 sekund na kliknięcie tego przycisku. Jeśli nie kliknie w określonym czasie, button zniknie.
+
+
+// 2*. Dodaj licznik upływających sekund na przycisku, żeby user widział ile pozostało mu czasu.
